b7b0fd26c4f60b55e444cb246f0499977f7b96a7
Thanmay Jayakumar <69724164+ThanmayJ@users.noreply.github.com>
2024-03-26 15:21:33 +0530

    Update Inference.py

--- Inference.py
+++ Inference.py
@@ -23,6 +23,8 @@ def InferOnCarb(args, model, tokenizer, device, dataset):
     predictions, _ = test(args, tokenizer, model, device, test_loader)
     final_df = pd.DataFrame({'Input Text':dataset["test"], 'Generated Text':predictions})
     final_df.to_csv(os.path.join(args.output_dir,'carb_predictions.csv'))
+    convert_predictions_to_carb_format(predictions)
+    
         
 if __name__ == "__main__":  
     parser = argparse.ArgumentParser()
